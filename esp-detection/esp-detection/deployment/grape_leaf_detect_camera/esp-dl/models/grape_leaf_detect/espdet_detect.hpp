a4bc20c08939068e17a8afba5349dc97d55c69b2
Tsegay844 <tsegay844@gmail.com>
2025-12-27 16:32:09 +0000

    Add esp-dl directory and LOCAL_BUILD_FLASH_INSTRUCTIONS.md
    - Removed esp-dl as git submodule
    - Added esp-dl contents directly to repository
    - Added complete local build and flash instructions for Windows
    - Includes all grape_leaf_detect project files needed for building

---
+++ esp-detection/esp-detection/deployment/grape_leaf_detect_camera/esp-dl/models/grape_leaf_detect/espdet_detect.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "dl_detect_base.hpp"
+#include "dl_detect_espdet_postprocessor.hpp"
+
+namespace espdet_detect {
+class ESPDet : public dl::detect::DetectImpl {
+public:
+    ESPDet(const char *model_name);
+};
+} // namespace espdet_detect
+
+class ESPDetDetect : public dl::detect::DetectWrapper {
+public:
+    typedef enum {
+        ESPDET_PICO_224_224_GRAPE_LEAF,
+    } model_type_t;
+    ESPDetDetect(model_type_t model_type = static_cast<model_type_t>(CONFIG_DEFAULT_ESPDET_DETECT_MODEL));
+    void load_model();
+
+private:
+    model_type_t m_model_type;
+};
